c695f881e2a3f874b39aa94656674d282f62668b
Ddemkowicz <dakucp@gmail.com>
2023-05-14 16:18:14 +0200

    up do sprawdzenia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
 import Statistics from './Statistics/Statistics';
@@ -7,16 +6,18 @@ import friends from './FriendList/friends.json';
 import FriendList from './FriendList/FriendList';
 import transactions from './TransactionHistory/transactions.json';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <Fragment
+    <div
       style={{
-        height: '100vh',
-        display: 'flex',
+        height: '100%',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        backgroundColor: '#B1B1B1',
       }}
     >
       <Profile
@@ -29,6 +30,6 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory transactions={transactions} />;
-    </Fragment>
+    </div>
   );
 };
